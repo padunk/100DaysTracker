065bf4e5f4792dccebe4281175f887d1a17c790d
Padunk <abraham.agel@gmail.com>
2020-04-01 17:49:41 +0700

    adding necessary component

---
+++ client/src/components/AddChallenge/AddChallenge.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+interface Props {}
+
+const AddChallenge = (props: Props) => {
+    return <div>AddChallenge</div>;
+};
+
+export default AddChallenge;
---
+++ client/src/components/DetailChallenge/DetailChallenge.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+interface Props {}
+
+const DetailChallenge = (props: Props) => {
+    return <div>Detail Page</div>;
+};
+
+export default DetailChallenge;
