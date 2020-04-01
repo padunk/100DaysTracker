37831a639da70ceda00df3e965ef12a9d3f4f3a6
Padunk <abraham.agel@gmail.com>
2020-04-01 17:48:57 +0700

    feat(route): add react-router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+
+import Home from "./components/Home/Home";
+import AddChallenge from "./components/AddChallenge/AddChallenge";
+import DetailChallenge from "./components/DetailChallenge/DetailChallenge";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Switch>
+            <Route path='/' exact component={Home} />
+            <Route path='/add' exact component={AddChallenge} />
+            <Route path='/detail/:challengeID' component={DetailChallenge} />
+        </Switch>
+    );
 }
 
 export default App;
