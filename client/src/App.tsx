6d74a9784f7a21f88ca7b5d157a224453b0dadd5
Padunk <abraham.agel@gmail.com>
2020-04-02 17:40:25 +0700

    feat(skill): add skill list page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,7 @@ import { Switch, Route } from "react-router-dom";
 import Home from "./components/Home/Home";
 import AddChallenge from "./components/AddChallenge/AddChallenge";
 import DetailChallenge from "./components/DetailChallenge/DetailChallenge";
+import SkillList from "./components/SkillList/SkillList";
 
 function App() {
     return (
@@ -11,6 +12,7 @@ function App() {
             <Route path='/' exact component={Home} />
             <Route path='/add' exact component={AddChallenge} />
             <Route path='/detail/:challengeID' component={DetailChallenge} />
+            <Route path='/skill-list' component={SkillList} />
         </Switch>
     );
 }
---
+++ client/src/components/SkillList/SkillList.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {
+  
+}
+
+const SkillList = (props: Props) => {
+  return (
+    <div>
+      SkillList
+    </div>
+  )
+}
+
+export default SkillList
