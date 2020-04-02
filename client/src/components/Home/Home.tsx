6794018152570b0fb2297b7ddd5fa4db11f2b524
Padunk <abraham.agel@gmail.com>
2020-04-02 16:04:39 +0700

    feat(home): get all challenges working.

--- client/src/components/Home/Home.tsx
+++ client/src/components/Home/Home.tsx
@@ -1,9 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 interface Props {}
 
+interface IChallenge {
+    challenge_id: string;
+    title: string;
+    hash_tag: string;
+    goal: string;
+    date_created: number;
+}
+
 const Home = (props: Props) => {
-    return <div>Home</div>;
+    const [challengeList, setChallengeList] = useState<Array<IChallenge>>([]);
+
+    useEffect(() => {
+        async function getAllChallenges(url: string) {
+            try {
+                const response = await fetch(url).then(res => res.json());
+                setChallengeList(response);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+
+        getAllChallenges("http://192.168.168.17:5000");
+    }, []);
+
+    if (challengeList.length === 0) {
+        return <div>Loading</div>;
+    } else {
+        return (
+            <div>
+                <ul>
+                    {challengeList.map(challenge => {
+                        return (
+                            <li key={challenge.challenge_id}>
+                                <Link to={`detail/${challenge.challenge_id}`}>
+                                    <p>{challenge.title}</p>
+                                    <p>{challenge.goal}</p>
+                                </Link>
+                            </li>
+                        );
+                    })}
+                </ul>
+            </div>
+        );
+    }
 };
 
 export default Home;
--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 require("dotenv").config;
 const express = require("express");
-const path = require("path");
+const cors = require("cors");
 const bodyParser = require("body-parser");
 const methodOverride = require("method-override");
-const redis = require("redis");
+const sqlite3 = require("sqlite3").verbose();
 
 const APP = express();
 
@@ -11,12 +11,28 @@ const port = process.env.PORT || 5000;
 
 APP.use(express.json());
 APP.use(bodyParser.urlencoded({ extended: false }));
+APP.use(cors());
 APP.use(methodOverride("method"));
+
+const db = new sqlite3.Database(
+    "./database/challenges.db",
+    sqlite3.OPEN_READWRITE,
+    err => {
+        if (err) {
+            console.error(err.message);
+        } else {
+            console.log("Connecting to the in-memory SQLite Database");
+        }
+    }
+);
+
+APP.get("/", (req, res, next) => {
+    db.all("SELECT * FROM challenge", (err, rows) => {
+        console.log(rows);
+        res.end(JSON.stringify(rows));
+    });
+});
 
 APP.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
-
-APP.get('/', function (req, res, next) {
-    res.render('welcome home')
-})
