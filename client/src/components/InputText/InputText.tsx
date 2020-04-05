5d6ad25386c03b020c656bbfabbbb5920903bee5
Padunk <abraham.agel@gmail.com>
2020-04-05 13:04:46 +0700

    change TS props and onChange behavior

--- client/src/components/InputText/InputText.tsx
+++ client/src/components/InputText/InputText.tsx
@@ -1,22 +1,28 @@
 import React, { ReactElement } from "react";
+import classNames from "classnames";
 
 interface Props {
     name: string;
-    placeholder: string;
-    handleChange: Function;
-    req: boolean;
-    val: string;
+    handleChange: React.FormEventHandler<HTMLInputElement>;
+    customClass?: string;
+    placeholder?: string;
+    req?: boolean;
+    val?: string | undefined;
 }
 
+const defaultInputClass: string = "px-4 py-1 rounded-sm outline-none";
+
 export default function InputText(props: Props): ReactElement {
+    const inputClass = classNames(defaultInputClass, props.customClass);
     return (
         <input
-            className='just-self-start px-4 py-1 rounded-sm outline-none w-full'
+            className={inputClass}
             type='text'
             name={props.name}
             id={props.name}
             placeholder={props.placeholder}
-            onChange={event => props.handleChange(event)}
+            // onChange={(event) => props.handleChange(event)}
+            onChange={props.handleChange}
             required={props.req}
             value={props.val}
             autoComplete='off'
