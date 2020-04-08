1e55b4d41b1dcce8e19c14ab8f92a8a709ce305b
Padunk <abraham.agel@gmail.com>
2020-04-08 09:24:25 +0700

    feat(anim): add gsap

--- client/src/components/SubTitle/SubTitle.tsx
+++ client/src/components/SubTitle/SubTitle.tsx
@@ -1,4 +1,6 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement, useRef, useEffect } from "react";
+import { gsap } from "gsap";
+
 import Wrapper from "../Wrapper/Wrapper";
 
 interface Props {
@@ -7,9 +9,23 @@ interface Props {
 }
 
 function SubTitle({ subtitle, emoji }: Props): ReactElement {
+    let heading: any = useRef(null);
+
+    useEffect(() => {
+        gsap.from(heading, .8, {
+            delay: .5,
+            x: -64,
+            opacity: 0,
+            ease: "back",
+            autoAlpha: 0,
+        });
+    }, [heading]);
+
     return (
         <Wrapper customClass='max-w-full'>
-            <h2 className='text-center text-2xl font-bold'>
+            <h2
+                className='text-center text-2xl font-bold opacity-100'
+                ref={(el) => (heading = el)}>
                 <span role='img' aria-label='emoji'>
                     {emoji}
                 </span>{" "}
