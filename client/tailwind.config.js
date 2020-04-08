a168fadd84ef9c62c11b1392fc0fe8bafd8072c0
Padunk <abraham.agel@gmail.com>
2020-04-08 09:26:51 +0700

    add new rules

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -6,8 +6,36 @@ module.exports = {
         },
         extend: {
             gridTemplateColumns: {
-                'form': '1fr 2fr'
-            }
-        }
+                form: "1fr 2fr",
+            },
+        },
+        inset: {
+            "1/4": "25%",
+            "1/3": "33%",
+            "1/2": "50%",
+            "2/3": "67%",
+            "3/4": "100%",
+        },
+        backgroundPosition: {
+            bottom: "bottom",
+            "bottom-4": "center bottom 1rem",
+            center: "center",
+            left: "left",
+            "left-bottom": "left bottom",
+            "left-top": "left top",
+            right: "right",
+            "right-bottom": "right bottom",
+            "right-top": "right top",
+            top: "top",
+            "top-2": "center top 2rem",
+            "bg-lg": "55vw 5rem",
+        },
+        backgroundSize: {
+            "1/4": "25%",
+            "1/3": "33%",
+            "1/2": "50%",
+            "2/3": "67%",
+            "3/4": "100%",
+        },
     },
 };
