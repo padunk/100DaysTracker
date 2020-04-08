1cb31ce1e1b00f32af79ebd340ef00cdfdb81697
Padunk <abraham.agel@gmail.com>
2020-04-08 09:26:00 +0700

    feat(comp): + subtitle, loading, divider > gap

--- client/src/components/DetailChallenge/DetailChallenge.tsx
+++ client/src/components/DetailChallenge/DetailChallenge.tsx
@@ -1,7 +1,11 @@
 import React, { useEffect, useState, Fragment } from "react";
-import Divider from "../Divider/Divider";
+import { gsap } from "gsap";
+
+import Gap from "../Gap/Gap";
 import AddDetail from "../AddDetail/AddDetail";
 import { BASE_URL } from "../../base/baseURL";
+import SubTitle from "../SubTitle/SubTitle";
+import Loading from "../../Loading/Loading";
 
 interface Props {
     match: {
@@ -42,16 +46,32 @@ const DetailChallenge = (props: Props) => {
         getAllDetails(`${BASE_URL}/detail/${id}`);
     }, [id]);
 
+    // GSAP ANIMATION
+    // useEffect(() => {
+    //     gsap.from(".daily", {
+    //         opacity: 0,
+    //         y: 120,
+    //         ease: "back",
+    //         stagger: 0.4,
+    //         duration: 1.4,
+    //     });
+    // }, [document.querySelectorAll('.daily')]);
+
     if (detailList.length === 0) {
-        return <div>Loading</div>;
+        return (
+            <div>
+                <Gap className='h-10' />
+                <Loading />
+            </div>
+        );
     } else if (detailList[0] === null) {
         return (
             <Fragment>
-                <Divider space={10} />
+                <Gap className='h-10' />
                 <h2 className='text-center text-pink-600 text-4xl'>
                     Start your first day now!
                 </h2>
-                <Divider space={10} />
+                <Gap className='h-10' />
                 <AddDetail
                     id={id}
                     hashtag={hashtag}
@@ -62,18 +82,20 @@ const DetailChallenge = (props: Props) => {
     } else {
         return (
             <Fragment>
-                <Divider space={5} />
+                <Gap className='h-5' />
+                <SubTitle subtitle='Track your daily progress' emoji='💻' />
+                <Gap className='h-5' />
                 <AddDetail
                     id={id}
                     hashtag={hashtag}
                     getAllDetails={getAllDetails}
                 />
-                <Divider space={6} />
+                <Gap className='h-6' />
                 <ol className='max-w-md mx-auto bg-teal-300 rounded-lg overflow-hidden'>
                     {detailList.map((detail, i) => {
                         return (
                             <li
-                                className='px-6 py-4 w-full border-b-2 border-teal-400'
+                                className='daily px-6 py-4 w-full border-b-2 border-teal-400'
                                 key={`${detail.parent_id}-${i}`}>
                                 <p className='first-line-bold whitespace-pre-wrap'>
                                     {detail.tweet}
@@ -82,7 +104,7 @@ const DetailChallenge = (props: Props) => {
                         );
                     })}
                 </ol>
-                <Divider space={8} />
+                <Gap className='h-8' />
             </Fragment>
         );
     }
