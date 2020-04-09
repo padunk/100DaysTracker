fdbfb16996736f7bc826f42bf79abcf1b9f0dfa4
Padunk <abraham.agel@gmail.com>
2020-04-09 17:56:50 +0700

    feat(style): update styling and navigation

--- client/src/Loading/Loading.tsx
+++ client/src/Loading/Loading.tsx
@@ -3,23 +3,24 @@ import React, { ReactElement, useEffect, useRef } from "react";
 import { gsap } from "gsap";
 
 function Loading(): ReactElement {
-    let loadingElement: any = useRef(null);
+  let loadingElement: any = useRef(null);
 
-    useEffect(() => {
-        let animation = gsap.timeline();
-        animation.to(loadingElement, {
-            rotate: 9999,
-            duration: 2,
-            repeat: -1,
-            repeatDelay: 0.5,
-            ease: "back",
-        });
-    }, [loadingElement]);
-    return (
-        <div
-            ref={(el) => (loadingElement = el)}
-            className='loading w-16 h-16 mx-auto rounded-full border-double border-8 b-t-red b-r-blue b-b-green b-l-yellow'></div>
-    );
+  useEffect(() => {
+    let animation = gsap.timeline();
+    animation.to(loadingElement, {
+      rotate: 1999,
+      duration: 2,
+      repeat: -1,
+      repeatDelay: 0.3,
+      ease: "back"
+    });
+  }, [loadingElement]);
+  return (
+    <div
+      ref={el => (loadingElement = el)}
+      className="loading w-16 h-16 mx-auto rounded-full border-double border-8 b-top-pink-500 b-r-white b-bottom-pink-500 b-l-white"
+    ></div>
+  );
 }
 
 export default Loading;
--- client/src/components/SkillCard/SkillCard.tsx
+++ client/src/components/SkillCard/SkillCard.tsx
@@ -9,84 +9,84 @@ import SvgUpdateArrow from "../../icons/UpdateArrow";
 import SvgCheckmark from "../../icons/Checkmark";
 
 interface ISkills {
-    skill_id: string;
-    skill_name: string;
-    new_skill: number;
-    progress_skill: number;
-    complete_skill: number;
-    date_created: number;
+  skill_id: string;
+  skill_name: string;
+  new_skill: number;
+  progress_skill: number;
+  complete_skill: number;
+  date_created: number;
 }
 
 interface Props {
-    id: number;
-    title: string;
-    skills: Array<ISkills>;
-    handleClick: any;
+  id: number;
+  title: string;
+  skills: Array<ISkills>;
+  handleClick: any;
 }
 
 function SkillDetail({ title, skills, id, handleClick }: Props): ReactElement {
-    return (
-        <Wrapper customClass='flex-1'>
-            <Wrapper customClass='bg-gray-800 text-white h-10 flex items-center px-4 border border-gray-100'>
-                <h3>{title}</h3>
-            </Wrapper>
-            <ul>
-                {skills.map((skill: ISkills, idx: number) => {
-                    const listClassNames = classNames(
-                        "flex flex-wrap justify-between items-center py-2 px-4",
-                        { "bg-gray-400": idx % 2 !== 0 }
-                    );
-                    return (
-                        <li key={skill.skill_id} className={listClassNames}>
-                            <span className='text-lg'>{skill.skill_name}</span>
-                            <div className='flex flex-wrap'>
-                                {id === 1 && (
-                                    <SvgShuttle
-                                        id={skill.skill_id}
-                                        fill='#3182ce'
-                                        className='w-4 h-4'
-                                        onClick={handleClick}
-                                    />
-                                )}
-                                {id === 2 && (
-                                    <>
-                                        <SvgCheckmark
-                                            id={skill.skill_id}
-                                            fill='#48bb78'
-                                            className='w-4 h-4'
-                                            onClick={handleClick}
-                                        />
-                                        <Gap className='w-4' />
-                                        <SvgUpdateArrow
-                                            id={skill.skill_id}
-                                            fill='#9f7aea'
-                                            className='w-4 h-4'
-                                            onClick={handleClick}
-                                        />
-                                    </>
-                                )}
-                                {id === 3 && (
-                                    <SvgUpdateArrow
-                                        id={skill.skill_id}
-                                        fill='#9f7aea'
-                                        className='w-4 h-4'
-                                        onClick={handleClick}
-                                    />
-                                )}
-                                <Gap className='w-4' />
-                                <SvgDelete
-                                    id={skill.skill_id}
-                                    fill='#e53e3e'
-                                    className='w-4 h-4'
-                                    onClick={handleClick}
-                                />
-                            </div>
-                        </li>
-                    );
-                })}
-            </ul>
-        </Wrapper>
-    );
+  return (
+    <Wrapper customClass="flex-1 pt-4 max-w-sm mx-auto">
+      <Wrapper customClass="bg-gray-800 text-white h-10 flex items-center px-4 border border-gray-100">
+        <h3>{title}</h3>
+      </Wrapper>
+      <ul>
+        {skills.map((skill: ISkills, idx: number) => {
+          const listClassNames = classNames(
+            "flex flex-wrap justify-between items-center py-2 px-4",
+            { "bg-gray-400": idx % 2 !== 0, "bg-gray-100": idx % 2 === 0 }
+          );
+          return (
+            <li key={skill.skill_id} className={listClassNames}>
+              <span className="text-lg">{skill.skill_name}</span>
+              <div className="flex flex-wrap">
+                {id === 1 && (
+                  <SvgShuttle
+                    id={skill.skill_id}
+                    fill="#3182ce"
+                    className="w-4 h-4 cursor-pointer hover-nav"
+                    onClick={handleClick}
+                  />
+                )}
+                {id === 2 && (
+                  <>
+                    <SvgCheckmark
+                      id={skill.skill_id}
+                      fill="#48bb78"
+                      className="w-4 h-4 cursor-pointer hover-nav"
+                      onClick={handleClick}
+                    />
+                    <Gap className="w-4" />
+                    <SvgUpdateArrow
+                      id={skill.skill_id}
+                      fill="#9f7aea"
+                      className="w-4 h-4 cursor-pointer hover-nav"
+                      onClick={handleClick}
+                    />
+                  </>
+                )}
+                {id === 3 && (
+                  <SvgUpdateArrow
+                    id={skill.skill_id}
+                    fill="#9f7aea"
+                    className="w-4 h-4 cursor-pointer hover-nav"
+                    onClick={handleClick}
+                  />
+                )}
+                <Gap className="w-4" />
+                <SvgDelete
+                  id={skill.skill_id}
+                  fill="#e53e3e"
+                  className="w-4 h-4 cursor-pointer hover-nav"
+                  onClick={handleClick}
+                />
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </Wrapper>
+  );
 }
 
 export default SkillDetail;
