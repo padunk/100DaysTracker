f9bdf89f05e769620d5cf52611aa7c23c3900e53
Padunk <abraham.agel@gmail.com>
2020-04-09 18:00:59 +0700

    feat(): update styling and error handling

--- client/src/components/AddChallenge/AddChallenge.tsx
+++ client/src/components/AddChallenge/AddChallenge.tsx
@@ -10,92 +10,95 @@ import SubTitle from "../SubTitle/SubTitle";
 interface Props {}
 
 const AddChallenge = (props: Props) => {
-    const inputWrapperClass =
-        "grid grid-cols-1 col-gap-4 px-4 sm:grid-cols-form";
+  const inputWrapperClass = "grid grid-cols-1 col-gap-4 px-4 sm:grid-cols-form";
 
-    const [title, setTitle] = useState<string | undefined>("");
-    const [hashtag, setHashtag] = useState<string | undefined>("");
-    const [goal, setGoal] = useState<string | undefined>("");
+  const [title, setTitle] = useState<string | undefined>("");
+  const [hashtag, setHashtag] = useState<string | undefined>("");
+  const [goal, setGoal] = useState<string | undefined>("");
 
-    const handleSubmit = async (event: any) => {
-        event.preventDefault();
-        try {
-            await fetch(`${BASE_URL}/add`, {
-                method: "POST",
-                mode: "cors",
-                credentials: "same-origin",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({ title, hashtag, goal }),
-            }).then(() => {
-                console.log("success add data to server");
-                setTitle("");
-                setHashtag("");
-                setGoal("");
-            });
-        } catch (error) {
-            console.error(error);
-        }
-    };
-    return (
-        <div>
-            <Gap className='h-8' />
-            <SubTitle subtitle='Add new challenge' emoji='🔥' />
-            <Gap className='h-5' />
-            <form
-                action='post'
-                className='container mx-auto bg-teal-400 py-4 rounded-lg max-w-md shadow-lg'
-                onSubmit={handleSubmit}>
-                <Wrapper customClass={inputWrapperClass}>
-                    <InputLabel for='title' />
-                    <InputText
-                        name='title'
-                        req={true}
-                        val={title}
-                        placeholder='Learn JavaScript'
-                        handleChange={(
-                            event: React.FormEvent<HTMLInputElement>
-                        ): void => setTitle(event.currentTarget.value)}
-                    />
-                </Wrapper>
-                <Gap className='h-2' />
-                <Wrapper customClass={inputWrapperClass}>
-                    <InputLabel for='hashtag' />
-                    <InputText
-                        name='hashtag'
-                        req={true}
-                        val={hashtag}
-                        placeholder='#100DaysOfCode'
-                        handleChange={(
-                            event: React.FormEvent<HTMLInputElement>
-                        ): void => setHashtag(event.currentTarget.value)}
-                    />
-                </Wrapper>
-                <Gap className='h-2' />
-                <Wrapper customClass={inputWrapperClass}>
-                    <InputLabel for='personalGoal' />
-                    <InputText
-                        name='personalGoal'
-                        req={false}
-                        val={goal}
-                        placeholder='Cool Web Developer'
-                        handleChange={(
-                            event: React.FormEvent<HTMLInputElement>
-                        ): void => setGoal(event.currentTarget.value)}
-                    />
-                </Wrapper>
-                <Gap className='h-5' />
-                <Wrapper customClass='flex justify-center'>
-                    <Button
-                        type='submit'
-                        customClass='bg-teal-700 text-teal-100'>
-                        Add Challenge
-                    </Button>
-                </Wrapper>
-            </form>
-        </div>
-    );
+  const handleSubmit = async (event: any) => {
+    event.preventDefault();
+    try {
+      await fetch(`${BASE_URL}/add`, {
+        method: "POST",
+        mode: "cors",
+        credentials: "same-origin",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ title, hashtag, goal })
+      })
+        .then(() => {
+          setTitle("");
+          setHashtag("");
+          setGoal("");
+        })
+        .catch(err => {
+          if (err) {
+            throw new Error(err.message);
+          }
+        });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  return (
+    <Wrapper customClass='px-2'>
+      <Gap className="h-8" />
+      <SubTitle subtitle="Add new challenge" emoji="🔥" />
+      <Gap className="h-5" />
+      <form
+        action="post"
+        className="container mx-auto bg-teal-400 py-4 rounded-lg max-w-md shadow-lg"
+        onSubmit={handleSubmit}
+      >
+        <Wrapper customClass={inputWrapperClass}>
+          <InputLabel for="title" />
+          <InputText
+            name="title"
+            req={true}
+            val={title}
+            placeholder="Learn JavaScript"
+            handleChange={(event: React.FormEvent<HTMLInputElement>): void =>
+              setTitle(event.currentTarget.value)
+            }
+          />
+        </Wrapper>
+        <Gap className="h-2" />
+        <Wrapper customClass={inputWrapperClass}>
+          <InputLabel for="hashtag" />
+          <InputText
+            name="hashtag"
+            req={true}
+            val={hashtag}
+            placeholder="#100DaysOfCode"
+            handleChange={(event: React.FormEvent<HTMLInputElement>): void =>
+              setHashtag(event.currentTarget.value)
+            }
+          />
+        </Wrapper>
+        <Gap className="h-2" />
+        <Wrapper customClass={inputWrapperClass}>
+          <InputLabel for="personalGoal" />
+          <InputText
+            name="personalGoal"
+            req={false}
+            val={goal}
+            placeholder="Cool Web Developer"
+            handleChange={(event: React.FormEvent<HTMLInputElement>): void =>
+              setGoal(event.currentTarget.value)
+            }
+          />
+        </Wrapper>
+        <Gap className="h-5" />
+        <Wrapper customClass="flex justify-center">
+          <Button type="submit" customClass="bg-teal-700 text-teal-100">
+            Add Challenge
+          </Button>
+        </Wrapper>
+      </form>
+    </Wrapper>
+  );
 };
 
 export default AddChallenge;
--- client/src/components/Skills/Skills.tsx
+++ client/src/components/Skills/Skills.tsx
@@ -1,194 +1,210 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Fragment, useRef } from "react";
 import AddSkill from "../AddSkill/AddSkill";
 import Gap from "../Gap/Gap";
 import SubTitle from "../SubTitle/SubTitle";
 import Wrapper from "../Wrapper/Wrapper";
 import { BASE_URL } from "../../base/baseURL";
 import SkillCard from "../SkillCard/SkillCard";
+import ServerStatusText from "../ServerStatusText/ServerStatusText";
 
 interface Props {}
 
 interface ISkills {
-    skill_id: string;
-    skill_name: string;
-    new_skill: number;
-    progress_skill: number;
-    complete_skill: number;
-    date_created: number;
+  skill_id: string;
+  skill_name: string;
+  new_skill: number;
+  progress_skill: number;
+  complete_skill: number;
+  date_created: number;
 }
 
 const Skills = (props: Props) => {
-    const [newSkill, setNewSkill] = useState<string | undefined>("");
-    const [allSkills, setAllSkills] = useState<Array<ISkills>>([]);
-    const [errorMessage, setErrorMessage] = useState<string>("");
+  const [newSkill, setNewSkill] = useState<string | undefined>("");
+  const [allSkills, setAllSkills] = useState<Array<ISkills>>([]);
+  const [message, setMessage] = useState<string>("");
+  const [status, setStatus] = useState("");
+  let refreshContent: any = useRef(null);
 
-    async function getAllSkills(url: string) {
-        try {
-            const response = await fetch(`${url}/skills`).then((r) => {
-                if (!r.ok) {
-                    throw new Error(
-                        `HTTP error, status = ${response.status}: ${response.statusText}`
-                    );
-                }
-                return r.json();
-            });
-            setAllSkills(response);
-        } catch (error) {
-            setErrorMessage(error.message);
+  async function getAllSkills(url: string) {
+    try {
+      const response = await fetch(`${url}/skills`).then(r => {
+        if (!r.ok) {
+          setStatus("" + r.status);
+          throw new Error(
+            `HTTP error, status = ${response.status}: ${response.statusText}`
+          );
         }
+        return r.json();
+      });
+      setAllSkills(response);
+    } catch (error) {
+      setMessage(error.message);
     }
+  }
 
-    useEffect(() => {
-        getAllSkills(BASE_URL);
-    }, []);
+  useEffect(() => {
+    getAllSkills(BASE_URL);
+  }, []);
 
-    const handleChange = (event: React.FormEvent<HTMLInputElement>): void =>
-        setNewSkill(event.currentTarget.value);
+  const handleChange = (event: React.FormEvent<HTMLInputElement>): void =>
+    setNewSkill(event.currentTarget.value);
 
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
-        event.preventDefault();
-        function postNewSkill(url: string, data: any) {
-            fetch(`${url}/skills`, {
-                method: "POST",
-                mode: "cors",
-                credentials: "same-origin",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(data),
-            })
-                .then((response) => {
-                    console.log(response);
-                    if (!response.ok) {
-                        throw new Error(
-                            `HTTP error, status = ${response.status}: ${response.statusText}`
-                        );
-                    }
-                })
-                .then(() => getAllSkills(BASE_URL))
-                .catch((error) => {
-                    setErrorMessage(error.message);
-                });
-        }
-
-        postNewSkill(BASE_URL, { newSkill });
-    };
-
-    function deleteSkill(url: string, id: string) {
-        fetch(`${url}/skills`, {
-            method: "DELETE",
-            mode: "cors",
-            credentials: "same-origin",
-            headers: {
-                Accept: "application/json, application/text",
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ id }),
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
+    event.preventDefault();
+    function postNewSkill(url: string, data: any) {
+      fetch(`${url}/skills`, {
+        method: "POST",
+        mode: "cors",
+        credentials: "same-origin",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+      })
+        .then(response => {
+          if (!response.ok) {
+            setStatus("" + response.status);
+            throw new Error(
+              `HTTP error, status = ${response.status}: ${response.statusText}`
+            );
+          }
+          return response;
         })
-            .then((response) => {
-                console.log(response);
-                if (!response.ok) {
-                    throw new Error(
-                        `HTTP error, status = ${response.status}: ${response.statusText}`
-                    );
-                }
-            })
-            .then(() => getAllSkills(BASE_URL))
-            .catch((error) => {
-                setErrorMessage(error.message);
-            });
-    }
-
-    function updateSkillProgress(url: string, id: string, method: string) {
-        const updateSkill: any = {};
-        updateSkill.id = id;
-
-        if (method === "Delete") {
-            console.log("delete");
-            deleteSkill(BASE_URL, updateSkill.id);
-            return;
-        }
-
-        if (method === "Start") {
-            updateSkill.new_skill = 0;
-            updateSkill.progress_skill = 1;
-            updateSkill.complete_skill = 0;
-        } else if (method === "Restart") {
-            updateSkill.new_skill = 1;
-            updateSkill.progress_skill = 0;
-            updateSkill.complete_skill = 0;
-        } else if (method === "Complete") {
-            updateSkill.new_skill = 0;
-            updateSkill.progress_skill = 0;
-            updateSkill.complete_skill = 1;
-        }
-
-        fetch(`${url}/skills`, {
-            method: "PATCH",
-            mode: "cors",
-            credentials: "same-origin",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(updateSkill),
+        .then((data: any) => {
+          setNewSkill("");
+          setMessage("Succesfully adding data to the server.");
+          setStatus("" + data.status);
+          refreshContent.current = setTimeout(() => {
+            setMessage("");
+            getAllSkills(BASE_URL);
+          }, 3000);
         })
-            .then((response) => {
-                console.log(response);
-                if (!response.ok) {
-                    throw new Error(
-                        `HTTP error, status = ${response.status}: ${response.statusText}`
-                    );
-                }
-            })
-            .then(() => getAllSkills(BASE_URL))
-            .catch((error) => {
-                setErrorMessage(error.message);
-            });
+        .catch(error => {
+          setMessage(error.message);
+        });
     }
 
-    const handleClick = (e: any) => {
-        const { id, textContent: method } = e.currentTarget;
-        updateSkillProgress(BASE_URL, id, method);
-    };
+    postNewSkill(BASE_URL, { newSkill });
+  };
 
-    return (
-        <div>
-            <Gap className='h-5' />
-            <SubTitle subtitle='My New SuperSkills' emoji='🚀' />
-            <Gap className='h-10' />
-            <AddSkill
-                handleChange={handleChange}
-                handleSubmit={handleSubmit}
-                skill={newSkill}
-            />
-            <Gap className='h-8' />
-            <p>{errorMessage}</p>
-            <Wrapper customClass='flex flex-row flex-wrap mx-auto justify-around mx-auto max-w-full w-8/12 container'>
-                <SkillCard
-                    id={1}
-                    handleClick={handleClick}
-                    title='Wish Skills'
-                    skills={allSkills.filter((skill) => skill.new_skill === 1)}
-                />
-                <SkillCard
-                    id={2}
-                    handleClick={handleClick}
-                    title='In Progress'
-                    skills={allSkills.filter(
-                        (skill) => skill.progress_skill === 1
-                    )}
-                />
-                <SkillCard
-                    id={3}
-                    handleClick={handleClick}
-                    title='Complete'
-                    skills={allSkills.filter(
-                        (skill) => skill.complete_skill === 1
-                    )}
-                />
-            </Wrapper>
-        </div>
-    );
+  useEffect(() => {
+    return () => {
+      clearTimeout(refreshContent);
+    };
+  }, []);
+
+  function deleteSkill(url: string, id: string) {
+    fetch(`${url}/skills`, {
+      method: "DELETE",
+      mode: "cors",
+      credentials: "same-origin",
+      headers: {
+        Accept: "application/json, application/text",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ id })
+    })
+      .then(response => {
+        console.log(response);
+        if (!response.ok) {
+          throw new Error(
+            `HTTP error, status = ${response.status}: ${response.statusText}`
+          );
+        }
+      })
+      .then(() => getAllSkills(BASE_URL))
+      .catch(error => {
+        setMessage(error.message);
+      });
+  }
+
+  function updateSkillProgress(url: string, id: string, method: string) {
+    const updateSkill: any = {};
+    updateSkill.id = id;
+
+    if (method === "Delete") {
+      console.log("delete");
+      deleteSkill(BASE_URL, updateSkill.id);
+      return;
+    }
+
+    if (method === "Start") {
+      updateSkill.new_skill = 0;
+      updateSkill.progress_skill = 1;
+      updateSkill.complete_skill = 0;
+    } else if (method === "Restart") {
+      updateSkill.new_skill = 1;
+      updateSkill.progress_skill = 0;
+      updateSkill.complete_skill = 0;
+    } else if (method === "Complete") {
+      updateSkill.new_skill = 0;
+      updateSkill.progress_skill = 0;
+      updateSkill.complete_skill = 1;
+    }
+
+    fetch(`${url}/skills`, {
+      method: "PATCH",
+      mode: "cors",
+      credentials: "same-origin",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(updateSkill)
+    })
+      .then(response => {
+        console.log(response);
+        if (!response.ok) {
+          throw new Error(
+            `HTTP error, status = ${response.status}: ${response.statusText}`
+          );
+        }
+      })
+      .then(() => getAllSkills(BASE_URL))
+      .catch(error => {
+        setMessage(error.message);
+      });
+  }
+
+  const handleClick = (e: any) => {
+    const { id, textContent: method } = e.currentTarget;
+    updateSkillProgress(BASE_URL, id, method);
+  };
+
+  return (
+    <Fragment>
+      <Gap className="h-5" />
+      <SubTitle subtitle="My New SuperSkills" emoji="🚀" />
+      <Gap className="h-4" />
+      <AddSkill
+        handleChange={handleChange}
+        handleSubmit={handleSubmit}
+        skill={newSkill}
+      />
+      <Gap className="h-2" />
+      <ServerStatusText message={message} status={status} />
+      <Wrapper customClass="md:flex max-w-3xl mx-auto">
+        <SkillCard
+          id={1}
+          handleClick={handleClick}
+          title="Wish Skills"
+          skills={allSkills.filter(skill => skill.new_skill === 1)}
+        />
+        <SkillCard
+          id={2}
+          handleClick={handleClick}
+          title="In Progress"
+          skills={allSkills.filter(skill => skill.progress_skill === 1)}
+        />
+        <SkillCard
+          id={3}
+          handleClick={handleClick}
+          title="Complete"
+          skills={allSkills.filter(skill => skill.complete_skill === 1)}
+        />
+      </Wrapper>
+      <Gap className="pb-40" />
+    </Fragment>
+  );
 };
 
 export default Skills;
