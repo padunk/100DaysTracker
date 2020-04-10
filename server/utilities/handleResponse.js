71e064916f18175a403d19ee6899d245fc436d37
Padunk <abraham.agel@gmail.com>
2020-04-10 12:47:04 +0700

    feat(res): + response for redirect

--- server/utilities/handleResponse.js
+++ server/utilities/handleResponse.js
@@ -1,14 +1,18 @@
 function handleError(res, err) {
-    console.error(err.message);
-    res.status(500).send(err.message);
+  console.error(err.message);
+  res.status(500).send(err.message);
 }
 
 function handleSuccess(res, responseCode, text, id = "", data = null) {
-    console.log(text + id);
-    res.status(responseCode).json(data);
+  console.log(text + id);
+  if (("" + responseCode)[0] === "3") {
+    res.redirect(responseCode, "/");
+    return;
+  }
+  res.status(responseCode).json(data);
 }
 
 module.exports = {
-    handleError,
-    handleSuccess,
+  handleError,
+  handleSuccess
 };
